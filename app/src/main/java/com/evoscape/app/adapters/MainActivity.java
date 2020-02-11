ec687e75ee5ff83e3ca9598adfefdbb135258366
Travis Teerink <Travis_Teerink@outlook.com>
2020-02-11 05:20:28 -0800

    Init, pre-dev branch merge

--- app/src/main/java/com/evoscape/app/adapters/MainActivity.java
+++ app/src/main/java/com/evoscape/app/adapters/MainActivity.java
@@ -2,7 +2,10 @@ package com.evoscape.app.adapters;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.evoscape.app.R;
 
@@ -12,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnLoginFacebook).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), ServiceActivity.class));
+            }
+        });
     }
 }
